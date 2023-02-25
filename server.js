d038c89839e680a52f5fe0467a10692d30e955b2
Eric dos Santos <eubdsantos@gmail.com>
2023-02-25 14:57:15 -0300

    Add filteredFunction

--- server.js
+++ server.js
@@ -41,7 +41,13 @@ app.put("/clients/:id", function(req, res){
 });
 
 
-app.delete("/clients/:id", function(req, res){});
+app.delete("/clients/:id", function(req, res){
+  const { id } = req.params;
+  const clientsFiltered = data.filter(client => client.id != id )
+
+  res.json(clientsFiltered);
+
+});
 
 
 app.listen(3000, function() {
